d702eab06cbf59a2fe443272b40a448011857423
Anthoni Carvalho Da Silva <anthonicarvalho30@gmail.com>
2025-12-10 17:52:38 -0300

    Subjects

---
+++ ES6 Module/index.js
@@ -0,0 +1,12 @@
+import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
+
+
+const circumference = getCircumference(10);
+const area = getArea(20)
+const volume = getVolume(10)
+
+console.log(`${PI.toFixed(2)}cm`);
+console.log(`${circumference.toFixed(2)}cm`);
+console.log(`${area.toFixed(2)}cm`);
+console.log(`${volume.toFixed(2)}cm`);
+
