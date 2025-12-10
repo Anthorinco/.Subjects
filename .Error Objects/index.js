9878bd2d142c7e48a7bd64478d9365b3506c9650
Anthorinco <anthonicarvalho30@gmail.com>
2025-12-10 19:26:56 -0300

    Mais Subjects!!

---
+++ .Error Objects/index.js
@@ -0,0 +1,12 @@
+try{
+    console.log("Hello!");
+}
+catch(error){
+    console.error(error);
+}
+
+finally{
+    console.log("This always executes");
+}
+
+console.log("You have reached the end!");
