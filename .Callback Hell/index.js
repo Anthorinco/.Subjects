8d8f178477dce791687be07f0dff5e99c44c8026
Anthorinco <anthonicarvalho30@gmail.com>
2025-12-15 23:19:06 -0300

    HELL!!

---
+++ .Callback Hell/index.js
@@ -0,0 +1,63 @@
+function task1(cb){setTimeout(()=>{console.log("Task 1 complete");cb();},500);}
+function task2(cb){setTimeout(()=>{console.log("Task 2 complete");cb();},600);}
+function task3(cb){setTimeout(()=>{console.log("Task 3 complete");cb();},700);}
+function task4(cb){setTimeout(()=>{console.log("Task 4 complete");cb();},800);}
+function task5(cb){setTimeout(()=>{console.log("Task 5 complete");cb();},900);}
+function task6(cb){setTimeout(()=>{console.log("Task 6 complete");cb();},1000);}
+function task7(cb){setTimeout(()=>{console.log("Task 7 complete");cb();},1100);}
+function task8(cb){setTimeout(()=>{console.log("Task 8 complete");cb();},1200);}
+function task9(cb){setTimeout(()=>{console.log("Task 9 complete");cb();},1300);}
+function task10(cb){setTimeout(()=>{console.log("Task 10 complete");cb();},1400);}
+function task11(cb){setTimeout(()=>{console.log("Task 11 complete");cb();},1500);}
+function task12(cb){setTimeout(()=>{console.log("Task 12 complete");cb();},1600);}
+function task13(cb){setTimeout(()=>{console.log("Task 13 complete");cb();},1700);}
+function task14(cb){setTimeout(()=>{console.log("Task 14 complete");cb();},1800);}
+function task15(cb){setTimeout(()=>{console.log("Task 15 complete");cb();},1900);}
+function task16(cb){setTimeout(()=>{console.log("Task 16 complete");cb();},2000);}
+function task17(cb){setTimeout(()=>{console.log("Task 17 complete");cb();},2100);}
+function task18(cb){setTimeout(()=>{console.log("Task 18 complete");cb();},2200);}
+function task19(cb){setTimeout(()=>{console.log("Task 19 complete");cb();},2300);}
+function task20(cb){setTimeout(()=>{console.log("Task 20 complete");cb();},2400);}
+
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => {
+        task5(() => {
+          task6(() => {
+            task7(() => {
+              task8(() => {
+                task9(() => {
+                  task10(() => {
+                    task11(() => {
+                      task12(() => {
+                        task13(() => {
+                          task14(() => {
+                            task15(() => {
+                              task16(() => {
+                                task17(() => {
+                                  task18(() => {
+                                    task19(() => {
+                                      task20(() => {
+                                        console.log("You've completed all 20 tasks");
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+// DAMN!!
